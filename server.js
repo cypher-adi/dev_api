456295db232f833164ed689abc5bc938460dbd09
Cypher <adityasaha444@gmail.com>
2020-07-20 17:31:07 +0530

    handle unhadled rejections

--- server.js
+++ server.js
@@ -24,7 +24,14 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  //Close server and exit process
+  server.close(() => process.exit(1));
+});
