60e3b64d4db45cb027d5cf2ccc1f84172facda98
Cypher <adityasaha444@gmail.com>
2020-08-19 17:11:47 +0530

    + create bootcamp

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,8 +1,10 @@
+const Bootcamp = require('../models/Bootcamp');
+
 //  @desc     Get all bootcamps
 //  @route    GET /api/v1/bootcamps
 //  @access   Public
 exports.getBootcamps = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+  res.status(200).json({ success: true, msg: 'Show all bootcamps' });
 };
 
 //  @desc     Get single bootcamp
@@ -17,8 +19,17 @@ exports.getBootcamp = (req, res, next) => {
 //  @desc     Create new bootcamp
 //  @route    POST /api/v1/bootcamps
 //  @access   Private
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //  @desc     Update bootcamp
--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -43,11 +43,9 @@ const BootcampSchema = new mongoose.Schema({
     type: {
       type: String,
       enum: ['Point'],
-      required: true,
     },
     coordinates: {
       type: [Number],
-      required: true,
       index: '2dsphere',
     },
     formattedAddress: String,
