a1063201a438b336da38fb83615f3b542831b52a
Cypher <adityasaha444@gmail.com>
2020-08-31 20:37:14 +0530

    Add a course

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,6 +1,7 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
+const Bootcamp = require('../models/Bootcamp');
 
 //  @desc     Get all courses
 //  @route    GET /api/v1/courses
@@ -41,6 +42,28 @@ exports.getCourse = asyncHandler(async (req, res, next) => {
       new ErrorResponse(`No course with id of ${req.params.id}`, 404)
     );
   }
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+//  @desc     Add course
+//  @route    POST /api/v1/bootcamps/:bootcampId/courses
+//  @access   Private
+exports.addCourse = asyncHandler(async (req, res, next) => {
+  req.body.bootcamp = req.params.bootcampId;
+
+  const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`No bootcamp with id of ${req.params.bootcampId}`, 404)
+    );
+  }
+
+  const course = await Course.create(req.body);
 
   res.status(200).json({
     success: true,
