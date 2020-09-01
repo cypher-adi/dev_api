03fe254a2ae845fbfafbb1f1e25eba075030509c
Cypher <adityasaha444@gmail.com>
2020-09-01 19:30:59 +0530

    delete a course

--- routes/courses.js
+++ routes/courses.js
@@ -4,11 +4,12 @@ const {
   getCourse,
   addCourse,
   updateCourse,
+  deleteCourse,
 } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
