69c351fd6a79eca3375cd6f842fed4a36785469b
Cypher <adityasaha444@gmail.com>
2020-09-04 20:15:27 +0530

    bootcamp user relationship

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -15,7 +15,6 @@ const BootcampSchema = new mongoose.Schema(
     description: {
       type: String,
       required: [true, 'Please add a description'],
-      unique: true,
       trim: true,
       maxlength: [500, 'Description cannot be more than 500 characters'],
     },
@@ -100,6 +99,11 @@ const BootcampSchema = new mongoose.Schema(
     createdAt: {
       type: Date,
       default: Date.now,
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: true,
     },
   },
   {
