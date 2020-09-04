adf6a009526449ee820a7ba45f488ff7cd420850
Cypher <adityasaha444@gmail.com>
2020-09-04 20:50:31 +0530

    course ownership + averageCost error fix

--- models/Course.js
+++ models/Course.js
@@ -36,6 +36,11 @@ const CourseSchema = new mongoose.Schema({
     ref: 'Bootcamp',
     required: true,
   },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true,
+  },
 });
 
 // Static method to get avg of course tuitions
@@ -55,9 +60,15 @@ CourseSchema.statics.getAverageCost = async function (bootcampId) {
   ]);
 
   try {
-    await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
-      averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
-    });
+    if (obj[0]) {
+      await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+        averageCost: Math.ceil(obj[0].averageCost / 10) * 10,
+      });
+    } else {
+      await this.model('Bootcamp').findByIdAndUpdate(bootcampId, {
+        averageCost: undefined,
+      });
+    }
   } catch (err) {
     console.log(err);
   }
