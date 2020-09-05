7197dae0a063a5bac0e2c34f704029312bcc66a0
Cypher <adityasaha444@gmail.com>
2020-09-05 08:53:14 +0530

    resetPassowrdToken generated

--- controllers/auth.js
+++ controllers/auth.js
@@ -49,8 +49,40 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res);
 });
 
-// Fet token from model, create cookie and send reponse
+//  @desc     Get current logged in user
+//  @route    GET /api/v1/auth/me
+//  @access   Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
 
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+//  @desc     Forget password
+//  @route    POST /api/v1/auth/forgetpassword
+//  @access   Private
+exports.forgotpassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  // Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+    data: user,
+  });
+});
+
+// Get token from model, create cookie and send reponse
 const sendTokenResponse = (user, statusCode, res) => {
   // Create token
   const token = user.getSignedJwtToken();
@@ -71,15 +103,3 @@ const sendTokenResponse = (user, statusCode, res) => {
     token,
   });
 };
-
-//  @desc     Get current logged in user
-//  @route    GET /api/v1/auth/me
-//  @access   Private
-exports.getMe = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.user.id);
-
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
-});
